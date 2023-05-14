c03f946e4e60d1ecbcdf121debb9abaa16c86fcf
Mahady Hasan Fahim <109776849+fmahadyBD@users.noreply.github.com>
2023-05-14 11:02:32 +0600

    Update Helloworld.php

--- Day 24/day-24/app/classes/Helloworld.php
+++ Day 24/day-24/app/classes/Helloworld.php
@@ -8,13 +8,18 @@ class Helloworld
 {
 
 
-    public $message;
+    public $message,$firstName,$lastName;
     public function __construct()
     {
-        $this->message="hello world";
+        $this->message="hello in constructor";
     }
     public function index(){
-        echo $this->message;
+        $this->firstName="Mahady Hasan";
+        $this->lastName="Fahim";
+//        echo $this->message;
+        echo  gettype($this->firstName);
+
+       // echo'My Name: <br/>'. $this->firstName.'&nbsp; &nbsp;&nbsp;'.$this->lastName;// concate
     }
 
 }
