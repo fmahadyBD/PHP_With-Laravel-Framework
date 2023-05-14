ac2d3e5ed14923fa4811391b0ede3bb456c461e7
Mahady Hasan Fahim <109776849+fmahadyBD@users.noreply.github.com>
2023-05-14 09:16:08 +0600

    Day 23 upload

---
+++ day 23/now/index.php
@@ -0,0 +1,53 @@
+<?php
+//echo "Hello Mahady Hasan Fahim!";
+//function demo(){
+//    echo "Hey Fahim";
+//}
+//demo();
+
+//class demo{
+//    function one(){
+//        echo "Hey Mahady";
+//    }
+//
+//}
+//$obj=new demo();
+//$obj->one();
+
+
+
+
+//function demo(){
+//    echo "Hey Fahim";
+//}
+//
+//echo '<br/>=======================<br/>';
+//demo();
+
+//function demo(){
+//   $x=10;
+//   $y=20;
+//   $z=$x+$y;
+//   echo $z;
+//}
+//
+//echo '<br/>=======================<br/>';
+//demo();
+//echo '<br/>=======================<br/>';
+//demo();
+//echo '<br/>=======================<br/>';
+//demo();
+
+
+function demo($x,$y){
+
+    $z=$x+$y;
+    echo $z;
+}
+
+echo '<br/>=======================<br/>';
+demo(1,2);
+echo '<br/>=======================<br/>';
+demo(100,300);
+echo '<br/>=======================<br/>';
+demo(600,700);
